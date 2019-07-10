100a80fdb81b46b0623066e795ea11504fa0bd8d
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-09 17:15:04 -0700

    Add User input capability and grey button functionality

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -10,20 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Outlets
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    // Properties
+    private var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double.")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            switch calcMethod {
+            case "AC":
+                displayLabel.text = "0"
+                break
+            case "+/-":
+                displayLabel.text = String(number * -1)
+                break
+            case "%":
+                displayLabel.text = String(number / 100.0)
+                break
+            default:
+                break
+            }
+        }
+
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     
     }
 
