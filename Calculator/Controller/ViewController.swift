df032a30d9ccbf3c49aa1e52e3a4df21e69b9444
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-09 17:30:44 -0700

    Refactor code to use computed variables

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -15,15 +15,24 @@ class ViewController: UIViewController {
     
     // Properties
     private var isFinishedTypingNumber: Bool = true
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
-        
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double.")
-        }
         
         if let calcMethod = sender.currentTitle {
             switch calcMethod {
@@ -31,10 +40,10 @@ class ViewController: UIViewController {
                 displayLabel.text = "0"
                 break
             case "+/-":
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
                 break
             case "%":
-                displayLabel.text = String(number / 100.0)
+                displayValue /= 100.0
                 break
             default:
                 break
@@ -46,12 +55,23 @@ class ViewController: UIViewController {
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
+        guard let currentDisplayString = displayLabel.text else { fatalError() }
+        if currentDisplayString.contains(".") { return }
+        
         //What should happen when a number is entered into the keypad
         if let numValue = sender.currentTitle {
             if isFinishedTypingNumber {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Philip Yu on 7/9/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
